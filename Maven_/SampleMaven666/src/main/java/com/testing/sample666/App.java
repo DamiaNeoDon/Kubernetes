d94056eb460ae15c94575150511b0262f56e0316
DamiaNeoDon <83450931+DamiaNeoDon@users.noreply.github.com>
2022-11-15 11:31:14 +0100

    mvn archetype:generate POM builder

---
+++ Maven_/SampleMaven666/src/main/java/com/testing/sample666/App.java
@@ -0,0 +1,13 @@
+package com.testing.sample666;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
